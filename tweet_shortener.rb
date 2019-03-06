8d7775a8986ae43c3e9fcdc0ff9197ad73cad5f7
ansara.hooks <ansara.hooks@gmail.com>
2019-03-06 19:17:41 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,14 @@
-def word_substituter
-  dictionary = {
+def dictionary
+  dictionary_hash = {
     "hello": "hi",
+    "to": "2"
+    "two": "2"
+    "too": "2",
+    "for": "4"
+    "four": "4",
+    "be": "b",
+    "you": "u",
+    "at": "@",
+    "and": "&"
   }
 end
