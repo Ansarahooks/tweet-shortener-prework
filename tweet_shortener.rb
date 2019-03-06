d83301c959a5861b3381ffad3caa5b3ae6fcc9f6
ansara.hooks <ansara.hooks@gmail.com>
2019-03-06 19:23:25 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,10 +2,6 @@ def dictionary
   dictionary_hash = {
     "hello" => "hi",
     "to" => "2"
-    "two" => "2"
-    "too" => "2",
-    "for" => "4"
-    "four" => "4",
     "be" => "b",
     "you" => "u",
     "at" => "@",
