97b8b5a1f57fc3010fd3e21ba8ee0bd1bc296800
ansara.hooks <ansara.hooks@gmail.com>
2019-03-06 19:13:13 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,8 +2,8 @@ def word_substituter
   dictionary =
   {
     "hello": "hi",
-    ["to", "two", "too"]: "2",
-    ["for", "four"]: "4",
+    ["to", "two", "too"]=> "2",
+    ["for", "four"]=> "4",
     "be": "b",
     "you": "u",
     "at": "@",
