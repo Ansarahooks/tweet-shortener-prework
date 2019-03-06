08ac4fdcef9e8056fe2703c4f245734646a62074
ansara.hooks <ansara.hooks@gmail.com>
2019-03-06 19:46:19 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,12 +13,13 @@ def dictionary
   }
 end
 
-def word_substituter(string)
+def word_substituter (string)
   string.split.collect do |word|
-    if dictionary.keys.include?(word.downcase)
+  if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
     else
       word
     end
   end.join(" ")
-end 
+end
+
