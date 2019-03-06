ae30d3d3fbd826b75867e85eaf357d930dc70c5e
ansara.hooks <ansara.hooks@gmail.com>
2019-03-06 19:25:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,12 +1,13 @@
-dictionary = {
-    "hello" => "hi",
-    "to" => "2"
-    "two" => "2"
-    "too" => "2",
-    "for" => "4"
-    "four" => "4",
-    "be" => "b",
-    "you" => "u",
-    "at" => "@",
-    "and" => "&"
+def dictionary
+  dictionary_hash = {
+    "hello" => "hi"
   }
+
+```
+  "hello" becomes 'hi'
+  "to, two, too" become '2'
+  "for, four" become '4'
+  'be' becomes 'b'
+  'you' becomes 'u'
+  "at" becomes "@"
+  "and" becomes "&"
