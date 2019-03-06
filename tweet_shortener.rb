818368fbed7f1e52f12eedca8beae71c9bffa615
ansara.hooks <ansara.hooks@gmail.com>
2019-03-06 19:24:10 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,10 @@
 dictionary = {
     "hello" => "hi",
     "to" => "2"
+    "two" => "2"
+    "too" => "2",
+    "for" => "4"
+    "four" => "4",
     "be" => "b",
     "you" => "u",
     "at" => "@",
